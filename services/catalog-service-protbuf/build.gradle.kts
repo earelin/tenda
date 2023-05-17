cf808887121de75d56bd34501cd66a00fcd54e7f
Xavier Carriba <xavier.carriba@gmail.com>
2023-05-17 05:38:59 +0200

    Configuring protobuf project

--- services/catalog-service-protbuf/build.gradle.kts
+++ services/catalog-service-protbuf/build.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
     id("java-library")
-    id("com.google.protobuf") version "3.23.0"
+    id("com.google.protobuf") version "0.9.3"
 }
 
 group = "org.earelin.tenda"
-version = "1.0-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation("io.grpc:grpc-protobuf:1.55.1")
+    implementation("io.grpc:grpc-stub:1.55.1")
+    compileOnly("jakarta.annotation:jakarta.annotation-api:2.1.0")
 }
